51d43ad682aa3ddb771019b579257acbb7d488bf
jack <dfsgyg@se.sdf>
2025-07-25 10:33:08 +0800

    添加分析

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import '@rainbow-me/rainbowkit/styles.css';
 import { Providers } from '@/lib/providers';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        <Analytics />
+        {/* Ensure to include the Analytics component for tracking */}
       </body>
     </html>
   );
